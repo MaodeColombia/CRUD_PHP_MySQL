894a34c7a5321eda6825128cc24ae90586d5555e
Mauricio Martinez Orjuela <maodecolombia@gmail.com>
2022-09-22 02:34:40 -0500

    TEST: código funcional

--- index.php
+++ index.php
@@ -2,8 +2,15 @@
 //se requiere la conexión a penas inicie la aplicación
 include ("db.php")
 ?>
+
 <?php include("include/header.php")?>
         
-    <h1>Hello World</h1>
-
+        <h1>Hello World</h1>
+        <!-- Recargando la pagina
+        https://github.com/MaodeColombia/CRUD_PHP_MySQL/blob/master/extra/image/20220922023030.png
+        y viendo en código fuente
+        https://github.com/MaodeColombia/CRUD_PHP_MySQL/blob/master/extra/image/20220922022817.png
+        se observa que continua operativa la página web
+     -->
+        
 <?php include("include/footer.php")?>
