e47f51acf4b2b1c2b89356f206992d19b36ee837
Mauricio Martinez Orjuela <maodecolombia@gmail.com>
2022-09-22 02:25:35 -0500

    - Limpiar el index > footer

--- index.php
+++ index.php
@@ -3,10 +3,7 @@
 include ("db.php")
 ?>
 <?php include("include/header.php")?>
-        <h1>Hello World</h1>
+        
+    <h1>Hello World</h1>
 
-        <!-- JavaScript Bundle with Popper -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-
-    </body>
-</html>
+<?php include("include/footer.php")?>
