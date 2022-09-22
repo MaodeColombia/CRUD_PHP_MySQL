b0b943e4ac5ff6e128a947561aa6adea7bd35695
Mauricio Martinez Orjuela <maodecolombia@gmail.com>
2022-09-22 01:17:26 -0500

    Conexión con al servidor con mysqli_connect

--- db.php
+++ db.php
@@ -1,3 +1,18 @@
 <?php
-//el encargado de mantener la conexión
+// LA CONEXIÓN A SQL
+
+/* 
+* - De la biblioteca de PHP se usa el objeto    mysqli
+* - mysqli_connect abre una nueva conexión al servidor de MySQL
+* - https://www.php.net/manual/en/mysqli.construct.php
+*
+* YA ESTE ES EL OBJETO QUE GENERA LA CONEXIÓN
+*/ 
+
+mysqli_connect(
+  'localhost',
+  'root',
+  '',
+  'php_mysql_crud'
+);
 ?>
