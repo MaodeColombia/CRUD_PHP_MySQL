fb4d7584c0f12f49ea1b14c4a5b90e487163daa7
Mauricio Martinez Orjuela <maodecolombia@gmail.com>
2022-09-29 08:33:02 -0500

    Bootstrap .container

--- index.php
+++ index.php
@@ -5,6 +5,14 @@ include ("db.php")
 
 <?php include("includes/header.php")?>
         
-        <!-- Seccion donde se tendrá un formulario y una tabla  -->
+    <!-- Seccion donde se tendrá un formulario y una tabla  -->
+    <div>
+        <!-- contenedor de Bootstrap que permite centrar el contenido 
+        https://sutilweb.com/lenguajes/bootstrap/bootstrap-4/contenedores-en-bootstrap/ 
+        https://github.com/MaodeColombia/CRUD_PHP_MySQL/blob/master/extra/image/20220929082755_class='containerp-4'.jpg-->
+        <div class="container p-4">
+            
+        </div>
+    </div>
         
 <?php include("includes/footer.php")?>
