08fecfc1f68bb8ff4ef5230cb7bf25a75bef56b0
Mauricio Martinez Orjuela <maodecolombia@gmail.com>
2022-09-28 20:06:27 -0500

    Nota aclaratoria de los queries de creación de tablas

--- extra/CREATE_command.sql
+++ extra/CREATE_command.sql
@@ -23,3 +23,28 @@ CREATE TABLE task(
     para ver en linea de comando la tabla `task` usar el comando 
     DESCRIBE task;
 */
+
+/*  El siguiente query es generado por phpMyAdmin cuando se crea la tabla manualmente
+    
+    CREATE TABLE `php_mysql_crud`.`task` ( 
+    `id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT , 
+    `title` VARCHAR(255) NOT NULL , 
+    `description` TEXT , 
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ) ENGINE = InnoDB; 
+
+    https://github.com/MaodeColombia/CRUD_PHP_MySQL/blob/master/extra/image/20220928193313.jpg
+
+    y esto genera la misma tabla
+    +-------------+--------------+------+-----+---------------------+----------------+
+    | Field       | Type         | Null | Key | Default             | Extra          |
+    +-------------+--------------+------+-----+---------------------+----------------+
+    | id          | int(11)      | NO   | PRI | NULL                | auto_increment |
+    | title       | varchar(255) | NO   |     | NULL                |                |
+    | description | text         | YES  |     | NULL                |                |
+    | created_at  | timestamp    | NO   |     | current_timestamp() |                |
+    +-------------+--------------+------+-----+---------------------+----------------+
+ */
+/*  RECORDAR:
+        si se requiere borrar la tabla el query es
+        DROP TABLE `task`;
+*/
