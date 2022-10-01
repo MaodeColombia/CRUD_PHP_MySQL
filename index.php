2125465e4f1c059c9df9ad7ccf238c8509f06d07
Mauricio Martinez Orjuela <maodecolombia@gmail.com>
2022-10-01 00:00:53 -0500

    - Renombrado de archivo

--- index.php
+++ index.php
@@ -9,14 +9,14 @@ include ("db.php")
     <div>
         <!-- contenedor de Bootstrap que permite centrar el contenido 
         https://sutilweb.com/lenguajes/bootstrap/bootstrap-4/contenedores-en-bootstrap/ 
-        https://github.com/MaodeColombia/CRUD_PHP_MySQL/blob/master/extra/image/20220929082755_class=containerp-4.jpg -->
+        https://github.com/MaodeColombia/CRUD_PHP_MySQL/blob/master/extra/image/20220929082755_class='containerp-4'.jpg -->
         <div class="container p-4">
             <!-- https://www.w3schools.com/bootstrap/bootstrap_grid_system.asp
                 https://www.w3schools.com/bootstrap/bootstrap_grid_examples.asp
                 https://getbootstrap.com/docs/5.0/layout/grid/
                 https://github.com/MaodeColombia/CRUD_PHP_MySQL/blob/master/extra/image/20220929084148_col-md-4.jpg -->
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-4">  
                     <!-- https://www.w3schools.com/bootstrap5/bootstrap_cards.php
                         https://github.com/MaodeColombia/CRUD_PHP_MySQL/blob/master/extra/image/20220929093058_cardcard-body.jpg
                         https://getbootstrap.com/docs/5.0/components/card/ -->
