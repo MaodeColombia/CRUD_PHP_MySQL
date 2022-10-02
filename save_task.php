2ca85abeaaaac05815913261c93f28f668191cd9
Mauricio Martinez Orjuela <maodecolombia@gmail.com>
2022-10-02 17:11:59 -0500

    + Preparaciones para el almacenamiento a la DB

--- save_task.php
+++ save_task.php
@@ -1,12 +1,16 @@
 <!-- La variable superglobal $_POST de tipo array contiene todos los datos de POST -->
 <!-- isset — Determina si una variable está definida y no es null
 https://www.php.net/manual/es/function.isset.php -->
+
+
+<!-- Comprobada el envio de información, ahora lo que se requiere es almacenar la informacion en la Dabe de Datos para lo cual se requiere de la conexión del query -->
 <?php
+   include('db.php'); // recordando, en este archivo solamente está el objeto $conn que establece la conexión a la DB
+
     if (isset($_POST['save_task'])) {
-        $t = $_POST['title'];
-        $d = $_POST['description'];
-        echo $t;
-        echo '<br>';
-        echo $d;
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        
+        //$query 
     }
 ?>
