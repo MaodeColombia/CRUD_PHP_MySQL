b6498d719d0f1d3fbe5c542724ac98bf81677f61
Mauricio Martinez Orjuela <maodecolombia@gmail.com>
2022-10-02 13:53:03 -0500

    El input, textarea y boton

--- index.php
+++ index.php
@@ -27,21 +27,26 @@ include ("db.php")
                                 https://getbootstrap.com/docs/4.0/components/forms/
                                 https://www.w3schools.com/bootstrap5/bootstrap_forms.php -->
                             <div class="form-group">
-                                <input type="text">
+                                <!-- https://www.w3schools.com/html/html_forms.asp
+                                    name="title" especifica el nombre de la forma https://www.w3schools.com/html/html_forms_attributes.asp
+                                    class="form-control" clase de bootstrap https://getbootstrap.com/docs/4.0/components/forms/#form-controls
+                                    placeholder="Task title" https://www.w3schools.com/tags/att_input_placeholder.asp
+                                    autofocus para que cuando incie la pagina el cursor se ubique en este input -->
+                                <input type="text" name="title" class="form-control" placeholder="Task title" autofocus>
                             </div>
                             <div class="form-group">
-                                <textarea name="" id="" cols="" rows=""></textarea>
+                                <!-- textarea https://www.w3schools.com/tags/tag_textarea.asp
+                                    name="description"  El atributo de nombre es necesario para hacer referencia a los datos del formulario después de enviar el formulario, https://www.w3schools.com/tags/tag_textarea.asp
+                                    rows="2" El atributo de filas especifica la altura visible de un área de texto, en líneas, https://www.w3schools.com/tags/att_textarea_rows.asp
+                                    class="form-control" clase de bootstrap
+                                     -->
+                                <textarea name="description" rows="2" class="form-control" placeholder="Task description"></textarea>
                             </div>
                             <!-- botón de tipo "submit" para que cuando se haga clic ejecute el formulario 
-                                https://www.w3schools.com/tags/att_input_type_submit.asp 
+                                https://www.w3schools.com/tags/att_input_type_submit.asp
+                                https://getbootstrap.com/docs/4.0/components/buttons/ 
                                 https://github.com/MaodeColombia/CRUD_PHP_MySQL/blob/master/extra/image/20221002123720.png -->
-                            <input type="submit">
-                            <!-- este  input es netamente HTML, pero se podría cambiar por button Bootstrap
-                                https://getbootstrap.com/docs/4.6/components/forms/ 
-                                https://getbootstrap.com/docs/4.6/components/buttons/
-                                https://github.com/MaodeColombia/CRUD_PHP_MySQL/blob/master/extra/image/20221002123817.png -->
-                                <button type="submit" class="btn btn-primary">Primary</button>
-
+                            <input type="submit" class="btn btn-success btn-block" name="save_task" value="Save task">
                         </form>
                     </div>
                 </div>
